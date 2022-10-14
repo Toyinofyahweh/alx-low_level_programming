775ff6d9c22cce9bf8be268a08d353c598cbbb68
Toyinofyahweh <akintundeoluwatoyin972@gmail.com>
2022-10-14 14:42:03 -0700

    C programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - printing
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
