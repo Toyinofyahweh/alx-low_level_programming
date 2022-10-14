d5208d4d8c565ee327af42b4a498e47c4f120e75
Toyinofyahweh <akintundeoluwatoyin972@gmail.com>
2022-10-14 14:35:35 -0700

    C programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
