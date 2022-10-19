bb1ea41b63db363592c21322c1d9aa332f9f623f
Toyinofyahweh <akintundeoluwatoyin972@gmail.com>
2022-10-19 16:42:10 -0700

    C functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  *  print_times_table - Prints the n times table
@@ -26,6 +26,12 @@ void print_times_table(int n)
 				{
 					_putchar(32);
 					_putchar(32);
+					_putchar(op + 48);
+				}
+				else if (op <= 99)
+				{
+					_putchar(32);
+					_putchar((op / 10) + 48);
 					_putchar(op + 48);
 				}
 				else
