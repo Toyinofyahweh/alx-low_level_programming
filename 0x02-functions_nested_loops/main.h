43697fb16b42927c48cc322925a4463720dd69ae
Toyinofyahweh <akintundeoluwatoyin972@gmail.com>
2022-10-19 14:29:30 -0700

    C function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef main_h
 #define main_h
+
 extern int _putchar(char c);
 extern void print_alphabet(void);
 extern void print_alphabet_x10(void);
@@ -13,4 +14,5 @@ extern void times_table(void);
 extern int add(int, int);
 extern void print_to_98(int n);
 extern void print_times_table(int n);
+
 #endif
