4b24e4b88f3eb2b451a166fab4e849de8e7822ec
Toyinofyahweh <akintundeoluwatoyin972@gmail.com>
2022-10-20 10:41:09 -0700

    C debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,11 +15,11 @@ int largest_number(int a, int b, int c)
 	printf("972, -98, 0: \n");
 	scanf("%d %d %d", &a, &b, &c);
 
-	if (a > b && b > c)
+	if (a >= b && a >= c)
 	{
 		printf("%d is the largest number.\n", a);
 	}
-	else if (b > a && a > c)
+	else if (b >= a && b >= c)
 	{
 		printf("%d is the largest number.\n", b);
 	}
