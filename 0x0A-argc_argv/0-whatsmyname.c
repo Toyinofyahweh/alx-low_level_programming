d4a2aea6369fa59ba21be706873983d6621f4a3c
Toyinofyahweh <akintundeoluwatoyin972@gmail.com>
2022-11-04 14:13:05 -0700

    C arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%s\n", argv[0]);
+
+	exit(EXIT_SUCCESS);
+}
