4511f688bf25e3b266862090f9423263904ab6a9
Toyinofyahweh <akintundeoluwatoyin972@gmail.com>
2022-12-09 15:12:09 -0800

    C programming

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -310,43 +310,14 @@ int main(int _attribute((unused_)) argc, char *argv[])
 	check_elf(header->e_ident);
 	printf("ELF Header:\n");
 	print_magic(header->e_ident);
- 
-ass(header->e_ident);
- /**
- * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
- *
- * Return: 1 if it success. -1 if it fails.
- */
+	print_class(header->e_ident);
+	print_data(header->e_ident);
+	print_version(header->e_ident);
+	print_osabi(header->e_ident);
+	print_abi(header->e_ident);
+	print_type(header->e_type, header->e_ident);
 
-int create_file(const char *filename, char *text_content)
-{
-	int fd;
-	int nletters;
-	int rwr;
-
-	if (!filename)
-		return (-1);
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1)
-		return (-1);
-
-	if (!text_content)
-		text_content = "";
-
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
-	close(fd);
-
-	return (1);
+	free(header);
+	close_elf(o);
+	return (0);
 }
-
